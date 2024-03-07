a7ccf2f2da5104ae6dfbf31cd7221c8e09fce6cc
Jakub Kuźniar <kubkuzn@gmail.com>
2024-03-07 19:19:58 +0100

    Poprawienie wizualizacji maski

--- BCNB/segmentation/tools/visualise_mask.py
+++ BCNB/segmentation/tools/visualise_mask.py
@@ -4,9 +4,9 @@ import os
 
 os.environ["OPENCV_IO_MAX_IMAGE_PIXELS"] = str(3600000000)
 
-file = '/home/oogabooga/praktyki/BCNB/segmentation/data/images/1.jpg'
+file = '/home/oogabooga/praktyki/BCNB/segmentation/data/test-images/38.jpg'
 assert os.path.exists(file)
-mask_path = '/home/oogabooga/praktyki/BCNB/segmentation/data/masks/1.png'
+mask_path = '/home/oogabooga/praktyki/BCNB/segmentation/data/test-masks/38.png'
 
 mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
 img = cv2.imread(file)
@@ -17,7 +17,7 @@ alpha = np.where(mask == 0, 0, 255).astype(np.uint8)
 
 # utworzenie maski która ma wymiary takie jak obraz i nadanie  jej zielonego kolru oraz przezroczystości
 green_color = np.zeros_like(img)
-green_color[:, :, 1] = 64
+green_color[:, :, 1] = 32
 
 # nałożenie maski alpha na maskę green aby alpha była zielona
 alpha_colored = cv2.bitwise_and(green_color, green_color, mask=alpha)
