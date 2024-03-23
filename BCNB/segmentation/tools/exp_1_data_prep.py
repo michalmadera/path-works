3b27c9270747a65776794795e12afa494bc3e5ac
Jakub Kuźniar <kubkuzn@gmail.com>
2024-03-23 02:51:16 +0100

    Data prep zrobiony, przenoszenie plików do odpowiednich folderów

--- BCNB/segmentation/tools/exp_1_data_prep.py
+++ BCNB/segmentation/tools/exp_1_data_prep.py
@@ -1,6 +1,14 @@
+import shutil
+
 import create_masks as masker
 import image_tiles as tiler
 import os
+import re
+
+def sorted_alphanumeric(data):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in filter(None, re.split('([0-9]+)', key))]
+    return sorted(data, key=alphanum_key)
 
 def prepare_masked_images():
     masker.create_masks_for_folder("data/source-images",
@@ -15,7 +23,26 @@ def split_into_train_valid_test(no_train_images, no_val_images, no_test_images):
 # Take first no_train_images  from data/source-images and move them to data/train-images
 # Take next no_val_images from data/source-images and move them to data/val-images
 # Take next no_test_images from data/source-images and move them to data/test-images
+    source_dir = "data/source-images"
+    train_dir = "data/train-images"
+    val_dir = "data/val-images"
+    test_dir = "data/test-images"
+    files = sorted_alphanumeric(os.listdir(source_dir))
 
+    val_file_range = no_train_images + no_val_images
+    test_file_range = val_file_range + no_test_images
+
+    #przenoszenie pierwszych N plików do folderu train-images
+    for filename in files[:no_train_images]:
+        shutil.move(os.path.join(source_dir, filename), os.path.join(train_dir, filename))
+
+    # przenoszenie kolejnych M plików do folderu val-images
+    for filename in files[no_train_images:val_file_range]:
+        shutil.move(os.path.join(source_dir, filename), os.path.join(val_dir, filename))
+
+    # przenoszenie kolejnych X plików do folderu test-images
+    for filename in files[val_file_range:test_file_range]:
+        shutil.move(os.path.join(source_dir, filename), os.path.join(test_dir, filename))
 
 def split_to_tiles():
     tiler.split_to_tiles("data/train-images",
@@ -43,9 +70,8 @@ def split_to_tiles():
 
 if __name__ == '__main__':
 
-    prepare_masked_images()
-    split_into_train_valid_test(300, 50, 50)
-    split_to_tiles()
+    #prepare_masked_images()
+    split_into_train_valid_test(20, 5, 5)
+    #split_to_tiles()
 
-    print("Data prepared")
 
